124f911fe4d456a9c1f719335a23ebfaa53569f9
Kyle Daigle <kdaigle23@gmail.com>
2018-11-19 11:42:58 -0600

    Fixed login bug

--- src/java/truckingcompanymanagementsystem/UserAccounts.java
+++ src/java/truckingcompanymanagementsystem/UserAccounts.java
@@ -2,6 +2,7 @@
  * 
  */
 package truckingcompanymanagementsystem;
+
 import java.util.*;
 import java.io.*;
 import java.sql.*;
@@ -14,29 +15,22 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author justin
  */
+public final class UserAccounts {
 
-public final class UserAccounts 
-{
-    
-    private UserAccounts()
-    {
+    private UserAccounts() {
         //constructor
     }
-    
-    public static UserAccounts getInstance() 
-    {
+
+    public static UserAccounts getInstance() {
         return UserAccountsHolder.INSTANCE;
     }
-    
-    
-     private static class UserAccountsHolder 
-    { 
+
+    private static class UserAccountsHolder {
+
         private static final UserAccounts INSTANCE = new UserAccounts();
     }
-     
 
     Database connection = Database.getInstance();
-    
 
     static String stored_username;
     static String position;
@@ -45,99 +39,61 @@ public final class UserAccounts
     private int id;
     static boolean has_logged_in = false;
     static boolean user_authenticated = false;
-        
-    
-        
-    
-    
-    
-    
-    public void userAuthentication(String username, String password)
-    {
+
+    public void userAuthentication(String username, String password) {
         //User CurrentUser = new User();
         ResultSet login = null;
         String loginQuery = "SELECT username FROM Personnel_Data";
-        try
-        {
+        try {
             connection.startConnection();
             login = connection.getGenericResultSet(loginQuery);
-        }
-        catch(SQLException ex)
-        {
-            Logger.getLogger(UserAccounts.class.getName()).log(Level.SEVERE,null,ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(UserAccounts.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         boolean found = false;
         user_authenticated = false;
-        
-        try
-        {
-        while(login.next())
-        {
-            String database_username = login.getString("username");            
-            
-            if(username.equals(database_username))
-            {
-                found = true;
-                break;
-            }
-        }
-        
-        if(found)
-        {
-            
-            login = connection.getGenericResultSet("SELECT password, "
-                    + "employee_id_number, access FROM Personnel_Data WHERE username = '" 
-                    + username + "'");
-            while(login.next())
-            {
-                String database_password = login.getString("password");
-                id = login.getInt("employee_id_number");
-                access = login.getInt("access");
-                if(password.equals(database_password))
-                {
-                    user_authenticated = true;
-                    has_logged_in = true;
-                    stored_username = username;
-                    switch(access)
-                    {
-                        case 1:                           
-                            access_level = "full";
-                            break;
-                            
-                        case 2:
-                            access_level = "shipping";
-                            break;
-                            
-                        case 3:
-                            access_level = "driver";
-                            break;  
-                            
-                        case 4:
-                            access_level = "maint";
-                            break;
-                            
-                        
-                    }
+
+        try {
+            while (login.next()) {
+                String database_username = login.getString("username");
+
+                if (username.equals(database_username)) {
+                    found = true;
+                    break;
                 }
             }
-          
-        } 
-        
+
+            if (found) {
+
+                login = connection.getGenericResultSet("SELECT password, "
+                        + "employee_id_number, access FROM Personnel_Data WHERE username = '"
+                        + username + "'");
+                while (login.next()) {
+                    String database_password = login.getString("password");
+                    id = login.getInt("employee_id_number");
+                    access = login.getInt("access");
+                    if (password.equals(database_password)) {
+                        user_authenticated = true;
+                        has_logged_in = true;
+                        stored_username = username;
+                    }
+                }
+
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(UserAccounts.class.getName()).log(Level.SEVERE, null, ex);
         }
-        catch(SQLException ex)
-        {
-            Logger.getLogger(UserAccounts.class.getName()).log(Level.SEVERE,null,ex);
-        }
-        
-        
+
     }
-    
+
     public int getID() {
         return id;
     }
-    
-    
-    
-    
+
+    public int getAccess() {
+        return access;
+    }
+
 }
